b1c22b64fbcb89bc3b11a15bf7e865ee15f94455
icelvin <icelvin2k@gmail.com>
2023-07-13 03:09:53 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ * return - must return 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
