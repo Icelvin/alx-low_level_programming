66fdcef50834530823e9d1f6298a88bab3232bc0
icelvin <icelvin2k@gmail.com>
2023-07-13 03:40:10 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,14 @@
 #include<stdio.h>
-
+/**
+ * main - entry point
+ * Return: must return o(success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu\n",sizeof(char));
+	printf("Size of an int: %lu\n",sizeof(int));
+	printf("Size of a long int: %lu\n",sizeof(long int));
+        printf("Size of a long long int: %lu\n",sizeof(long long int));
+	printf("Size of a float: %lu\n",sizeof(float));
+	return (0);
+}
