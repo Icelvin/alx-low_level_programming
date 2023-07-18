ad2e8b34110601133e6b30dcb772b3599551e2f9
icelvin <icelvin2k@gmail.com>
2023-07-18 14:53:45 -0700

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *main-entry point
+  *Return: must return
+  */
+void print_alphabet_x10(void)
+{
+	int k;
+	int q;
+	for (k = 1; k <= 10; k++)
+	{
+		for (q = 97; q <= 122; q++)
+		{
+			_putchar(q);
+		}
+	}
+	putchar('\n');
