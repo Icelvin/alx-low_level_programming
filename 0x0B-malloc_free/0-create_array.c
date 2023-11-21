d68946c7128c149b730889d0ac02c92925fb9d00
icelvin <icelvin2k@gmail.com>
2023-11-21 00:41:41 -0800

    maalloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,47 +1,37 @@
-
 #include "main.h"
-		
-#include <stdlib.h>
-		
+
 /**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
- *
-		
- */
-		
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ *@size: Size of the array
+ *@c: Character to insert
+ *Return: NULL if size is zero or if it fails,
+ *pointer to array if everything is normal.
+
+ *FUNCTIONALITY *
+
+ * 1. First, we check if the size is 0. If it is, we return NULL.
+ * 2. Then, we allocate memory for the array.
+ * 3. If the allocation fails, we return NULL.
+ * 4. If the allocation succeeds, we loop through the array and set each element to the value of c.
+ * 5. Finally, we return the array.
+ * Time Complexity: O(n)
+
+*/
 char *create_array(unsigned int size, char c)
-		
 {
-		
-	char *str;
-		
-	unsigned int i;
-		
+	char *array;
+	unsigned int index;
 
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
+	if (size == 0)
 		return (NULL);
-		
+	array = malloc(sizeof(char) * size);
 
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
 }
-
